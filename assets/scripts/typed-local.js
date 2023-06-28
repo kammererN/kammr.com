c7910e9fac25f05a29d8a073548b0c2149a8dcda
Nick Kammerer <56482517+nxrada@users.noreply.github.com>
2023-06-28 18:35:08 -0400

    Fixed some Bootstrap layout quirks

---
+++ assets/scripts/typed-local.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.typing', {
+  strings: ['Programmer', 'Full-Stack Developer', 'ML Enthusiast', 'Undergraduate student', 'Nick', ''],
+  loop: true,
+  typeSpeed: 65,
+  backSpeed: 65,
+});
