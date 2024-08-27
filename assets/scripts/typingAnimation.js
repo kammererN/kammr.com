6147d14f22b610b50517cbd9092741a69c1b7405
Nick Kammerer <56482517+nxrada@users.noreply.github.com>
2024-08-26 21:51:16 -0400

    updated for august 2024

--- assets/scripts/typingAnimation.js
+++ assets/scripts/typingAnimation.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typing', {
-  strings: ['Programmer', 'Full-Stack Developer', 'ML Enthusiast', 'Undergraduate student', 'Nick', ''],
+  strings: ['Programmer', 'Full-Stack Developer', 'UNIX Enthusiast', 'B.S. Informatics/Software Development', 'Nick', ],
   loop: true,
   typeSpeed: 75,
   backSpeed: 75,
